d61aa8d3480d9d35bd0223c9d7e40627a287b9e6
Everton Solon <EvertonSolon@users.noreply.github.com>
2019-11-27 15:40:47 -0300

    LojaVirtual criada a partir da seção 07.

---
+++ Seção 07/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Visualizar()
+        {
+            var produto = GetProduto();
+
+            return View(produto);
+            //return new ContentResult {Content = "<h3>Produtos teste</h3>", ContentType = "text/html"};
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto
+            {
+                Id = 1,
+                Nome = "XBox",
+                Descricao = "Jogue bla bla",
+                Valor = 200.00M
+            };
+        }
+
+    }
+}
