fa69a18eba7fe4dc6d2c5bceb6f4a8e57c2c0131
Everton Solon <EvertonSolon@users.noreply.github.com>
2019-11-29 11:21:13 -0300

    Arquivos de contato.

--- Seção 07/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ Seção 07/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -12,5 +12,35 @@ namespace LojaVirtual.Controllers
         {
             return View();
         }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            var form = HttpContext.Request.Form;
+            var nome = HttpContext.Request.Form["nome"];
+            var email = HttpContext.Request.Form["email"];
+            var texto = HttpContext.Request.Form["texto"];
+            return new ContentResult { Content = "Dados recebidos com sucesso! " +
+                $"<p>Nome: ${nome}</p> <p>Email: {email}</p> <p>Texto: {texto}</p>", ContentType = "text/html" };
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
     }
 }
