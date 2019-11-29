129b20cb4d34af44c803ea891cf067cf5136b080
Everton Solon <EvertonSolon@users.noreply.github.com>
2019-11-29 13:45:09 -0300

    Arquivos de configuração de e-mail.

--- Seção 07/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ Seção 07/LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaVirtual.Controllers.Libraries.Email;
+using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LojaVirtual.Controllers
@@ -21,11 +23,26 @@ namespace LojaVirtual.Controllers
         public IActionResult ContatoAcao()
         {
             var form = HttpContext.Request.Form;
-            var nome = HttpContext.Request.Form["nome"];
-            var email = HttpContext.Request.Form["email"];
-            var texto = HttpContext.Request.Form["texto"];
-            return new ContentResult { Content = "Dados recebidos com sucesso! " +
-                $"<p>Nome: ${nome}</p> <p>Email: {email}</p> <p>Texto: {texto}</p>", ContentType = "text/html" };
+
+            var contato = new Contato
+            {
+                Nome = form["nome"],
+                Email = form["email"],
+                Texto = form["texto"]
+            };
+
+            ContatoEmail.EnviarContatoPorEmail(contato);
+
+            //Explicação
+            //var nome = HttpContext.Request.Form["nome"];
+            //var email = HttpContext.Request.Form["email"];
+            //var texto = HttpContext.Request.Form["texto"];
+            return new ContentResult
+            {
+                Content = "Dados recebidos com sucesso! " +
+                $"<p>Nome: {contato.Nome}</p> <p>Email: {contato.Email}</p> <p>Texto: {contato.Texto}</p>",
+                ContentType = "text/html"
+            };
         }
 
         public IActionResult Login()
