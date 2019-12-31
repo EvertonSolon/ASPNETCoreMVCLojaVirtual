3ca71f53c976ac152e21fa632a1ea05e33eb90c8
Everton Solon <EvertonSolon@users.noreply.github.com>
2019-12-31 18:53:59 -0300

    Inclusão de propriedades associativas na model Produtos e criação da classe Imagem. Migration executado para atualizar o banco de dados.

--- Seção 07/LojaVirtual/LojaVirtual/BaseDeDados/LojaVirtualContext.cs
+++ Seção 07/LojaVirtual/LojaVirtual/BaseDeDados/LojaVirtualContext.cs
@@ -20,6 +20,8 @@ namespace LojaVirtual.BaseDeDados
         public DbSet<NewsLetterEmail> NewsLetterEmails { get; set; }
         public DbSet<Colaborador> Colaboradores { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Imagem> Imagens { get; set; }
 
     }
 }
--- Seção 07/LojaVirtual/LojaVirtual/Modelos/Produto.cs
+++ Seção 07/LojaVirtual/LojaVirtual/Modelos/Produto.cs
@@ -1,4 +1,7 @@
 ﻿using LojaVirtual.Modelos.Bases;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LojaVirtual.Modelos
 {
@@ -6,5 +9,27 @@ namespace LojaVirtual.Modelos
     {
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        //Frete
+        public double Peso { get; set; }
+        public int Largura { get; set; }
+        public int Altura { get; set; }
+        public int Comprimento { get; set; }
+
+        /*
+         * EF - ORM  - Biblioteca que serve para unir o Banco de dados e POO. (ORM - Mapeamento de objetos <-> Relacionamentos)
+         *Fluent API -  Attributes
+         */
+
+        //Banco de dados - Relacionamentos entre tabelas
+        [ForeignKey("Categoria")]
+        public int CategoriaId { get; set; }
+
+        //POO - Associações entre objetos
+        //[ForeignKey("CategoriaId")] ou um ou outro
+        public virtual Categoria Categoria { get; set; }
+
+        public virtual ICollection<Imagem> IMagens { get; set; }
     }
 }
