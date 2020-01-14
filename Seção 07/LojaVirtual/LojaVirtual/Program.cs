8f988f128e4e1b673e63e1d34a9f0e17314ccb33
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-01-14 20:22:43 -0300

    Copmplemento do commit anteror.

--- Seção 07/LojaVirtual/LojaVirtual/Program.cs
+++ Seção 07/LojaVirtual/LojaVirtual/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaVirtual.Bibliotecas.Bug;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
